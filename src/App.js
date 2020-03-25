f02ab3a00ddcbeec30dfebaacdf14ec47fe4fa47
btamsevi <brandontamsevicius@gmail.com>
2020-03-25 11:33:59 -0500

    App now uses API to store data externally

--- src/App.js
+++ src/App.js
@@ -1,80 +1,144 @@
 import React from 'react';
+import axios from 'axios';
+import LoginOrLogoutButton from './loginOrLogoutButton/LoginOrLogoutButton';
 import AddItemBar from './addItemBar/AddItemBar';
 import ItemList from './itemList/ItemList';
-import {default as DefaultTodos} from './DefaultTodos';
+import { Auth0Context } from './contexts/auth0-context';
 
 import './App.css';
+
+
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.addTodo = this.addTodo.bind(this); 
-    this.resetTodo = this.resetTodo.bind(this);
+
+    this.addTodo = this.addTodo.bind(this);
+    this.resetTodos = this.resetTodos.bind(this);
     this.deleteCompletedTodos = this.deleteCompletedTodos.bind(this);
     this.toggle = this.toggle.bind(this);
-    this.state = {todos: DefaultTodos};
+
+    this.state = { todos: [], accessToken: null };
   }
 
-  addTodo(todo) {
+
+  async addTodo(description) {
     const todos = this.state.todos;
-    const newTodo = {text: todo, completed: false};
+    const isLoggedIn = !!this.context.user;
+    let newTodo = { description: description };
+
+    if (isLoggedIn) {
+      const url = '/tasks'
+      const { data: newTodoWithId } = await axios.post(url, newTodo);
+      newTodo = newTodoWithId;
+    }
+
     todos.push(newTodo);
-    this.setState({todos : todos});
-    localStorage.setItem('todos', JSON.stringify(todos));
+    this.setState({ todos: todos });
+
   }
 
-  resetTodo() {
+  resetTodos() {
+    const isLoggedIn = !!this.context.user;
     const todos = this.state.todos;
-    todos.forEach(todo => todo.completed = false);
-    this.setState({todos : todos});
+
+    todos.forEach(async (todo) => {
+      if (todo.isComplete === true) {
+        todo.isComplete = false;
+
+        if (isLoggedIn) {
+          const url = `/tasks/${todo.id}`;
+          await axios.put(url, todo)
+        }
+      }
+    });
+    this.setState({ todos: todos });
   }
 
   deleteCompletedTodos() {
-    const newTodos = []
-    this.state.todos.forEach(_todo => {
-      if(_todo.completed === false)
-        newTodos.push(_todo)
-      });
-    console.log(newTodos);
-    this.setState({todos : newTodos});
-    localStorage.setItem('todos', JSON.stringify(newTodos));
-  }
+    const isLoggedIn = !!this.context.user;
+    const incompleteTodos = [];
 
-  toggle(todo) {
-    const newTodos = this.state.todos.map((_todo) => {
-      if(todo === _todo)
-        return {
-          text: todo.text,
-          completed: !todo.completed
-        }
-      else
-        return _todo
+
+    this.state.todos.forEach(todo => {
+
+      if (!todo.isComplete) {
+        incompleteTodos.push(todo);
+      }
+
+      if (isLoggedIn && todo.isComplete) {
+        const url = `/tasks/${todo.id}`;
+
+        axios.delete(url);
+      }
     });
-    this.setState({todos : newTodos});
-    localStorage.setItem('todos', JSON.stringify(newTodos));
+    this.setState({ todos: incompleteTodos });
   }
 
-   componentDidMount() {
-    const savedTodosStrings = localStorage.getItem('todos');
-    if(savedTodosStrings) {
-      const savedTodos = JSON.parse(savedTodosStrings);
-      this.setState({todos : savedTodos});
-    }
+  toggle(clickedTodo) {
+    const isLoggedIn = !!this.context.user;
+    const taskId = clickedTodo.id;
+
+    const todos = this.state.todos.map(todo => {
+      if(todo === clickedTodo) {
+        todo.isComplete = !todo.isComplete
+
+        if (isLoggedIn) {
+          const url = `/tasks/${taskId}`;
+          axios.put(url, todo)
+        };
+      };
+      return todo;
+    });
+  
+    this.setState({todos : todos}); 
   }
 
-  render(){
+
+  render() {
     const todos = this.state.todos;
+    const { isLoading, user, getTokenSilently } = this.context;
+    const isLoggedIn = !!user;
 
-    return(
+    if (isLoggedIn && this.state.accessToken === null) {
+      getTokenSilently()
+        .then(accessToken => {
+          axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+          axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+          axios.get('/tasks')
+            .then(result => {
+              const { data: todos } = result;
+              this.setState({ todos: todos, accessToken: accessToken });
+            });
+        });
+    };
+
+    return (
       <div>
-          <h1>Todo List</h1>
-          <AddItemBar placeholder='Add Todo' buttonText='+' intermediateFunc={this.addTodo}/>
-          <button onClick={this.resetTodo}>Reset</button>
-          <button onClick={this.deleteCompletedTodos}>Delete</button>
-          <ItemList todos={todos} toggleFn={this.toggle}/>
+        {!isLoading
+          ? (
+            <>
+              <LoginOrLogoutButton />
+              <h1>Todo List</h1>
+
+              <AddItemBar placeholder='Add Todo'
+                buttonText='+'
+                intermediateFunc={this.addTodo}
+                resetTodosFn={this.resetTodos}
+                deleteCompletedTodosFn={this.deleteCompletedTodos} />
+
+              <ItemList todos={todos} toggleFn={this.toggle} />
+            </>
+          )
+
+          : <div>Loading...</div>}
       </div>
     );
   }
 }
+
+
+App.contextType = Auth0Context;
 export default App;
