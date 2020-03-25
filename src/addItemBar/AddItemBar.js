e2d25ac664a12954936d68cff8117f079a0c81a4
btamsevi <brandontamsevicius@gmail.com>
2020-03-25 11:36:42 -0500

    Item bar component now owns delete/reset buttons

--- src/addItemBar/AddItemBar.js
+++ src/addItemBar/AddItemBar.js
@@ -3,34 +3,46 @@ import React from 'react';
 class AddItemBar extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value : ''};
-        this.updateList = this.updateList.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+        this.state = { value: '' };
+
         this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.updateList = this.updateList.bind(this);
+        this.resetTodos = this.resetTodos.bind(this);
+        this.deleteCompletedTodos = this.deleteCompletedTodos.bind(this);
     }
 
-    handleKeyDown(e){
+    handleKeyDown(e) {
         if (e.key === 'Enter') {
-            this.props.intermediateFunc(this.state.value);
-            this.setState({value : ''});
+            this.updateList();
         }
     }
 
     handleChange(e) {
-        this.setState({value : e.target.value});
+        this.setState({ value: e.target.value });
     }
 
     updateList(e) {
-        this.intermediateFunc(this.state.value);
-        this.setState({value : ''});
+        this.props.intermediateFunc(this.state.value);
+        this.setState({ value: '' });
+    }
+
+    resetTodos() {
+        this.props.resetTodosFn();
+    }
+
+    deleteCompletedTodos() {
+        this.props.deleteCompletedTodosFn();
     }
 
     render() {
         const buttonText = this.props.buttonText;
-        return(
+        return (
             <div className='AddItemBar'>
-                <input type='text' value={this.state.value} onChange={this.handleChange} onKeyDown={this.handleKeyDown} placeholder={this.props.placeholder} size='40'/>
+                <input type='text' value={this.state.value} onChange={this.handleChange} onKeyDown={this.handleKeyDown} placeholder={this.props.placeholder} size='40' />
                 <button type='button' onClick={this.updateList}>{buttonText}</button>
+                <button onClick={this.resetTodos}>Reset</button>
+                <button onClick={this.deleteCompletedTodos}>Delete</button>
             </div>
 
         );
