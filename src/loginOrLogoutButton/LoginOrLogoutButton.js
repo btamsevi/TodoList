0bc02954cb0a361e1b9386ed7b42962631f94235
btamsevi <brandontamsevicius@gmail.com>
2020-03-25 11:37:37 -0500

    Created login button

---
+++ src/loginOrLogoutButton/LoginOrLogoutButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Auth0Context } from '../contexts/auth0-context';
+
+class LoginOrLogoutButton extends React.Component {
+    render() {
+        const { user, loginWithRedirect, logout } = this.context;
+
+
+    return(
+    <div>
+        {!user ? (<button onClick={loginWithRedirect}>Login</button>) : <button onClick={() => logout({ returnTo: window.location.origin })}>Logout</button>}
+    </div>
+    );
+    }
+}
+
+LoginOrLogoutButton.contextType = Auth0Context;
+export default LoginOrLogoutButton;
