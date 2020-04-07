861901bda105a39255cae4e1a080dc7d6cc80538
btamsevi <brandontamsevicius@gmail.com>
2020-04-07 14:47:30 -0500

    added loginWithRedirectFn

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import LoginOrLogoutButton from './loginOrLogoutButton/LoginOrLogoutButton';
 import AddItemBar from './addItemBar/AddItemBar';
 import ItemList from './itemList/ItemList';
 import { Auth0Context } from './contexts/auth0-context';
-
 import './App.css';
 
 
@@ -18,15 +17,15 @@ class App extends React.Component {
     this.resetTodos = this.resetTodos.bind(this);
     this.deleteCompletedTodos = this.deleteCompletedTodos.bind(this);
     this.toggle = this.toggle.bind(this);
+    this.loginWithRedirectFn = this.loginWithRedirectFn.bind(this);
 
     this.state = { todos: [], accessToken: null };
   }
 
-
   async addTodo(description) {
-    const todos = this.state.todos;
+    const todos = [...this.state.todos];
     const isLoggedIn = !!this.context.user;
-    let newTodo = { description: description };
+    let newTodo = { description: description, isComplete: false };
 
     if (isLoggedIn) {
       const url = '/tasks'
@@ -36,12 +35,11 @@ class App extends React.Component {
 
     todos.push(newTodo);
     this.setState({ todos: todos });
-
   }
 
   resetTodos() {
     const isLoggedIn = !!this.context.user;
-    const todos = this.state.todos;
+    const todos = [...this.state.todos];
 
     todos.forEach(async (todo) => {
       if (todo.isComplete === true) {
@@ -79,9 +77,10 @@ class App extends React.Component {
   toggle(clickedTodo) {
     const isLoggedIn = !!this.context.user;
     const taskId = clickedTodo.id;
+    const todos = [...this.state.todos];
 
-    const todos = this.state.todos.map(todo => {
-      if(todo === clickedTodo) {
+    const todosAfterToggle = todos.map(todo => {
+      if (todo === clickedTodo) {
         todo.isComplete = !todo.isComplete
 
         if (isLoggedIn) {
@@ -91,52 +90,49 @@ class App extends React.Component {
       };
       return todo;
     });
-  
-    this.setState({todos : todos}); 
+
+    this.setState({ todos: todosAfterToggle });
+  }
+
+  async loginWithRedirectFn() {
+    await this.context.loginWithRedirect();
+
+    const accessToken = await this.context.getTokenSilently();
+
+    axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+    axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+    const { data: todos } = await axios.get('/tasks');
+
+    this.setState({ todos: todos, accessToken: accessToken });
   }
 
 
-  render() {
-    const todos = this.state.todos;
-    const { isLoading, user, getTokenSilently } = this.context;
-    const isLoggedIn = !!user;
 
-    if (isLoggedIn && this.state.accessToken === null) {
-      getTokenSilently()
-        .then(accessToken => {
-          axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-          axios.defaults.headers.common['Content-Type'] = 'application/json';
+render() {
+  const todos = this.state.todos;
+  const { isLoading } = this.context;
 
-          axios.get('/tasks')
-            .then(result => {
-              const { data: todos } = result;
-              this.setState({ todos: todos, accessToken: accessToken });
-            });
-        });
-    };
+  return (
+    <div>
+      {!isLoading
+        ? (
+          <>
+            <LoginOrLogoutButton loginFn={this.loginWithRedirectFn} />
+            <h1>Todo List</h1>
 
-    return (
-      <div>
-        {!isLoading
-          ? (
-            <>
-              <LoginOrLogoutButton />
-              <h1>Todo List</h1>
+            <AddItemBar addTodo={this.addTodo}
+              resetTodosFn={this.resetTodos}
+              deleteCompletedTodosFn={this.deleteCompletedTodos} />
 
-              <AddItemBar placeholder='Add Todo'
-                buttonText='+'
-                intermediateFunc={this.addTodo}
-                resetTodosFn={this.resetTodos}
-                deleteCompletedTodosFn={this.deleteCompletedTodos} />
+            <ItemList todos={todos} toggleFn={this.toggle} />
+          </>
+        )
 
-              <ItemList todos={todos} toggleFn={this.toggle} />
-            </>
-          )
-
-          : <div>Loading...</div>}
-      </div>
-    );
-  }
+        : <div>Loading...</div>}
+    </div>
+  );
+}
 }
 
 
